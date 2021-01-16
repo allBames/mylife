7b0808944f8b350e81d05bc3ad2de083e412990f
balmasovas <balmasov@ek.apress.ru>
2021-01-17 00:03:23 +0500

    Initial commit

---
+++ src/main/java/ru/mylife/project/MylifeApplication.java
@@ -0,0 +1,14 @@
+package ru.mylife.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MylifeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MylifeApplication.class, args);
+    }
+
+}
+gfdsa
