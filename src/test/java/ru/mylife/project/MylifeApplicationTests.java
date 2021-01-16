ace8fafe8b2943d44d802665d459e90c1ed2debb
balmasovas <balmasov@ek.apress.ru>
2021-01-17 00:06:50 +0500

    d

---
+++ src/test/java/ru/mylife/project/MylifeApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.mylife.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MylifeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
